1230497858ebe9cdf1a798d05f60ee88497a7ff0
Mark Benjamin <MarkBenjamin1111111@gmail.com>
2016-03-23 11:06:28 +0000

    code tidy

--- js/coord-patients-view.js
+++ js/coord-patients-view.js
@@ -15,11 +15,6 @@ XDate, setTimeout, getDataSet*/
          */
         metrics = null,
         
-        /**
-         * The scheme used to create and render the grid
-         */
-        scheme,
-        
         PRINT_MODE = $("html").is(".before-print"),
         
         EMPTY_MARK = PRINT_MODE ? "" : "&#8212;",
@@ -91,19 +86,9 @@ XDate, setTimeout, getDataSet*/
     
     function renderPatientsView( container ) {
         $(container).empty();
-//        var loadingdiv = $("<div><h2>Loading</h2></div>");
-//        $(container).append(loadingdiv);
         
-//        createHeaderPatientsTable(container);
-        var sampleData = [
-            ["PATIENT, Anthony", "735", "30332", "404-894-2000", "North Avenue, Atlanta, GA", "info@gatech.edu", "2015-06-07"],
-            ["SMITH, John", "644", "30303", "123-456-7890", "Some Street, Atlanta, GA", "info@somedomain.com", "2016-01-17"],
-            ["PATIENT, Anthonia", "736", "30332", "404-894-2000", "North Avenue, Atlanta, GA", "info3@gatech.edu", "2015-06-07"],
-            ["SMITH, Joanne", "645", "30303", "123-456-7890", "Some Street, Atlanta, GA", "info3@somedomain.com", "2016-01-17"],["PATIENT, Tony", "737", "30332", "404-894-2000", "North Avenue, Atlanta, GA", "info7@gatech.edu", "2015-06-07"],
-            ["SMITH, Johnathan", "646", "30303", "123-456-7890", "Some Street, Atlanta, GA", "info7@somedomain.com", "2016-01-17"]
-        ];
         var thetable = $("<table></table>").addClass("display");
-        thetable.prop("id", "patient-table").prop("width","100%");
+        thetable.prop("id", "patient-table").prop("width", "100%");
         $(container).append(thetable);
         var thedatatable = $("#patient-table").DataTable( {
             columns: [
@@ -199,191 +184,7 @@ XDate, setTimeout, getDataSet*/
             }
         }
     }
-    
-    /**
-     * The scheme used to create and render the grid
-     */
-/*    scheme = {
-        header : {
-            rows : [
-                // Annotation
-                {
-                    label : "STR_12", // Annotation
-                    get   : function( entry, model ) {
-                        return entry.annotation ? 
-                            '<div class="annotation-wrap">' + 
-                            GC.Util.ellipsis(entry.annotation.txt, 6, 36, "...") + 
-                            '</div>' : 
-                            "&#8212;";
-                    },
-                    rowClass : "annotation",
-                    secondCell : '<a href="javascript:GC.App.viewAnnotations();" class="annotations-see-all">See all</a>'
-                },
-                
-                // Length
-                {
-                    label : "STR_2", // Length
-                    units : { metric : "cm", eng : "ft - in" },
-                    get   : getLength,
-                    rowClass : "length heading",
-                    printrow : 1,
-                    printColspan : 3
-                },
-                {
-                    label : "STR_9", // Percentile
-                    units : { metric : "%", eng : "%" },
-                    get   : function( entry, model ) {
-                        return getPercentile( entry, "lengthAndStature" );
-                    },
-                    rowClass : "length percentile",
-                    printrow : 2
-                },
-                {
-                    label : "STR_7", // Z Score
-                    units : { metric : "Z", eng : "Z" },
-                    get   : function( entry, model ) {
-                        return getZScore( entry, "lengthAndStature" );
-                    },
-                    rowClass : "length z-score",
-                    printrow : 2
-                },
-                {
-                    label : "STR_10", // Velocity
-                    units : { 
-                        metric : function() {
-                            return getVelocityUnits("cm");
-                        }, 
-                        eng : function() {
-                            return getVelocityUnits("in");
-                        }
-                    },
-                    get   : function( entry, model ) {
-                        return getVelocity( entry, "lengthAndStature" );
-                    },
-                    rowClass : "length velocity",
-                    printrow : 2
-                },
-                
-                
-                // Weight
-                {
-                    label : "STR_6", // Weight
-                    units : { metric : "kg", eng : "lb - oz" },
-                    get   : getWeight,
-                    rowClass : "weight heading",
-                    printrow : 1,
-                    printColspan : 3
-                },
-                {
-                    label : "STR_9", // Percentile
-                    units : { metric : "%", eng : "%" },
-                    get   : function( entry, model ) {
-                        return getPercentile( entry, "weight" );
-                    },
-                    rowClass : "weight percentile",
-                    printrow : 2
-                },
-                {
-                    label : "STR_7", // Z Score
-                    units : { metric : "Z", eng : "Z" },
-                    get   : function( entry, model ) {
-                        return getZScore( entry, "weight" );
-                    },
-                    rowClass : "weight z-score",
-                    printrow : 2
-                },
-                {
-                    label : "STR_10", // Velocity
-                    units : {
-                        metric : function() {
-                            return getVelocityUnits("kg");
-                        }, 
-                        eng : function() {
-                            return getVelocityUnits("lb");
-                        }
-                    },
-                    get   : function( entry, model ) {
-                        return getVelocity( entry, "weight" );
-                    },
-                    rowClass : "weight velocity",
-                    printrow : 2
-                },
-                
-                // Head C
-                {
-                    label : "STR_13", // Head C
-                    units : { metric : "cm", eng : "in" },
-                    get   : getHeadC,
-                    rowClass : "headc heading",
-                    printrow : 1
-                },
-                {
-                    label : "STR_9", // Percentile
-                    units : { metric : "%", eng : "%" },
-                    get   : function( entry, model ) {
-                        return getPercentile( entry, "headc" );
-                    },
-                    rowClass : "headc percentile"
-                },
-                {
-                    label : "STR_7", // Z Score
-                    units : { metric : "Z", eng : "Z" },
-                    get   : function( entry, model ) {
-                        return getZScore( entry, "headc" );
-                    },
-                    rowClass : "headc z-score"
-                },
-                {
-                    label : "STR_10", // Velocity
-                    units : { 
-                        metric : function() {
-                            return getVelocityUnits("cm");
-                        }, 
-                        eng : function() {
-                            return getVelocityUnits("in");
-                        }
-                    },
-                    get   : function( entry, model ) {
-                        return getVelocity( entry, "headc" );
-                    },
-                    rowClass : "headc velocity"
-                },
-                
-                // BMI
-                {
-                    label : "STR_14", // BMI
-                    units : { metric : "kg/m2", eng : "lb/ft2x703" },
-                    get   : getBMI,
-                    rowClass : "bmi heading",
-                    printrow : 1
-                },
-                
-                {
-                    label : "STR_11", // Bone Age
-                    units : { metric : "y - m", eng : "y - m" },
-                    get   : function( entry, model ) {
-                        if (entry.hasOwnProperty("boneAge")) {
-                            var time = new GC.TimeInterval();
-                            time.setMonths(entry.boneAge);
-                            return time.toString({
-                                "Years"   : "y", 
-                                "Year"    : "y", 
-                                "Months"  : "m", 
-                                "Month"   : "m", 
-                                "Weeks"   : false,
-                                "Days"    : false,
-                                separator : '<span class="unit-separator"></span>'
-                            });
-                        }
-                        return EMPTY_MARK;
-                    },
-                    rowClass : "bone-age heading",
-                    printrow : 1
-                }
-            ]
-        }
-    };
-*/    
+
      
     NS.PatientsView = {
         render : function() {
