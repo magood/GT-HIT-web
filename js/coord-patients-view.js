3f81c63dea9cdfd04a26ab4a62813a1bb16a04ba
Sangwhan Moon <sangwhan@iki.fi>
2016-04-10 20:41:35 +0900

    Switch active view to patient messages before reloading patient.
    This isn't pretty, as the message view will load first then show
    up again after reload, but works okay enough for now.

--- js/coord-patients-view.js
+++ js/coord-patients-view.js
@@ -91,6 +91,7 @@ XDate, setTimeout, getDataSet*/
             console.log(thedatatable.row(this).data());
             if (thedatatable.row(this).data()[1] == "") return;
             window.sessionStorage.setItem('patient_id', thedatatable.row(this).data()[1]);
+            GC.App.setViewType("psmessages");
             location.href = '/?patient=' + thedatatable.row(this).data()[1];
         });
 
